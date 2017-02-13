fb052bab3f01e559c53d76a834b8a14694ae7f98
Bartłomiej Potępa <bartek19@op.pl>
2017-02-13 20:33:53 +0100

    kodilla

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+  sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'style.css': 'style.sass'
+        }
+      }
+    },
+    watch: {
+    scripts: {
+        files: ['*.sass'],
+        tasks: ['sass'],
+        options: {
+            spawn: false,
+        },
+    } 
+}
+  });
+  // Load the plugins tasks 
+  grunt.loadNpmTasks('grunt-sass');
+   grunt.loadNpmTasks('grunt-contrib-watch');
+  // Default task(s).
+  grunt.registerTask('default', ["sass", "watch"]);
+};
